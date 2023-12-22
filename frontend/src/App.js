56d98a5774cb7b36eba6a6d0390e08bb06094aae
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-12-22 10:07:07 -0500

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+import  {useState,useEffect} from "react"
+
+import React from "react";
+function App() {
+  const [people,setPeople] = useState([])
+  
+  useEffect(()=>{
+    axios.get("/api").then(res=>setPeople(res.data))
+
+  },[])
+  return people.map((p,index)=>{
+    return <p key={index}>{p.id}{p.name}{p.age}</p>
+  })
+}
+
+export default App;
